83f1c3fc93344c0264223fbf37ac0198626e006b
Matthew Garrett <mjg59@coreos.com>
2016-12-03 17:02:07 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ import warnings
 
 dynamic_requires = []
 
-version = 0.1
+version = 0.2
 
 setup(
     name='broadlink',
-    version=0.1,
+    version=0.2,
     author='Matthew Garrett',
     author_email='mjg59@srcf.ucam.org',
     url='http://github.com/mjg59/python-broadlink',
