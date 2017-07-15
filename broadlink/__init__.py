2160488da23f54adc6dfc8728167385e2c59eb45
Eugene Schava <eschava@gmail.com>
2017-07-16 02:30:22 +0300

    sp2.get_energy

--- broadlink/__init__.py
+++ broadlink/__init__.py
@@ -386,6 +386,16 @@ class sp2(device):
         state = bool(ord(payload[0x4]))
       return state
 
+  def get_energy(self):
+    packet = bytearray([8, 0, 254, 1, 5, 1, 0, 0, 0, 45])
+    response = self.send_packet(0x6a, packet)
+    err = response[0x22] | (response[0x23] << 8)
+    if err == 0:
+      payload = self.decrypt(bytes(response[0x38:]))
+      energy = int(hex(ord(payload[7]) * 256 + ord(payload[6]))[2:]) + int(hex(ord(payload[5]))[2:])/100.0
+      return energy
+
+
 class a1(device):
   def __init__ (self, host, mac):
     device.__init__(self, host, mac)
