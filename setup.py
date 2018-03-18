8d3c9f1e9f7ad19f1c054ca3278670c8c9ea0eb1
Matthew Garrett <mjg59@google.com>
2018-03-18 14:51:34 -0700

    Version 0.7

--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ except ImportError as e:
 # For Hysen thermostatic heating controller
 dynamic_requires.append('PyCRC')
 
-version = 0.6
+version = 0.7
 
 setup(
     name='broadlink',
-    version=0.6,
+    version=0.7,
     author='Matthew Garrett',
     author_email='mjg59@srcf.ucam.org',
     url='http://github.com/mjg59/python-broadlink',
