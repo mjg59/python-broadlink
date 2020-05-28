abc00c7cfb96d867912922ff0816e5c0151c9a94
Felipe Martins Diel <felipe-diel@hotmail.com>
2020-05-28 03:38:44 -0300

    Clean up A1 sensor

--- broadlink/__init__.py
+++ broadlink/__init__.py
@@ -512,79 +512,39 @@ class sp2(device):
 
 
 class a1(device):
+
+    _SENSORS_AND_LEVELS = (
+        ('light', ('dark', 'dim', 'normal', 'bright')),
+        ('air_quality', ('excellent', 'good', 'normal', 'bad')),
+        ('noise_levels', ('quiet', 'normal', 'noisy')),
+    )
+
     def __init__(self, *args, **kwargs):
         device.__init__(self, *args, **kwargs)
         self.type = "A1"
 
     def check_sensors(self):
-        packet = bytearray(16)
-        packet[0] = 1
-        response = self.send_packet(0x6a, packet)
-        check_error(response[0x22:0x24])
-        data = {}
-        payload = self.decrypt(bytes(response[0x38:]))
-        if isinstance(payload[0x4], int):
-            data['temperature'] = (payload[0x4] * 10 + payload[0x5]) / 10.0
-            data['humidity'] = (payload[0x6] * 10 + payload[0x7]) / 10.0
-            light = payload[0x8]
-            air_quality = payload[0x0a]
-            noise = payload[0xc]
-        else:
-            data['temperature'] = (ord(payload[0x4]) * 10 + ord(payload[0x5])) / 10.0
-            data['humidity'] = (ord(payload[0x6]) * 10 + ord(payload[0x7])) / 10.0
-            light = ord(payload[0x8])
-            air_quality = ord(payload[0x0a])
-            noise = ord(payload[0xc])
-        if light == 0:
-            data['light'] = 'dark'
-        elif light == 1:
-            data['light'] = 'dim'
-        elif light == 2:
-            data['light'] = 'normal'
-        elif light == 3:
-            data['light'] = 'bright'
-        else:
-            data['light'] = 'unknown'
-        if air_quality == 0:
-            data['air_quality'] = 'excellent'
-        elif air_quality == 1:
-            data['air_quality'] = 'good'
-        elif air_quality == 2:
-            data['air_quality'] = 'normal'
-        elif air_quality == 3:
-            data['air_quality'] = 'bad'
-        else:
-            data['air_quality'] = 'unknown'
-        if noise == 0:
-            data['noise'] = 'quiet'
-        elif noise == 1:
-            data['noise'] = 'normal'
-        elif noise == 2:
-            data['noise'] = 'noisy'
-        else:
-            data['noise'] = 'unknown'
+        data = self.check_sensors_raw()
+        for sensor, levels in self._SENSORS_AND_LEVELS:
+            try:
+                data[sensor] = levels[data[sensor]]
+            except IndexError:
+                data[sensor] = 'unknown'
         return data
 
     def check_sensors_raw(self):
-        packet = bytearray(16)
-        packet[0] = 1
+        packet = bytearray([0x1])
         response = self.send_packet(0x6a, packet)
         check_error(response[0x22:0x24])
-        data = {}
         payload = self.decrypt(bytes(response[0x38:]))
-        if isinstance(payload[0x4], int):
-            data['temperature'] = (payload[0x4] * 10 + payload[0x5]) / 10.0
-            data['humidity'] = (payload[0x6] * 10 + payload[0x7]) / 10.0
-            data['light'] = payload[0x8]
-            data['air_quality'] = payload[0x0a]
-            data['noise'] = payload[0xc]
-        else:
-            data['temperature'] = (ord(payload[0x4]) * 10 + ord(payload[0x5])) / 10.0
-            data['humidity'] = (ord(payload[0x6]) * 10 + ord(payload[0x7])) / 10.0
-            data['light'] = ord(payload[0x8])
-            data['air_quality'] = ord(payload[0x0a])
-            data['noise'] = ord(payload[0xc])
-        return data
+        data = bytearray(payload[0x4:])
+        return {
+            'temperature': data[0x0] + data[0x1] / 10.0,
+            'humidity': data[0x2] + data[0x3] / 10.0,
+            'light': data[0x4],
+            'air_quality': data[0x6],
+            'noise': data[0x8],
+        }
 
 
 class rm(device):
