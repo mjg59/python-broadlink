814cd82aaf488b7090fc03af719052baf015dfb0
Felipe Martins Diel <felipe-diel@hotmail.com>
2020-09-15 22:55:37 -0300

    Create a SDKException for DNS errors

--- broadlink/helpers.py
+++ broadlink/helpers.py
@@ -1,6 +1,8 @@
 """Helper functions."""
 import socket
 
+from .exceptions import exception
+
 
 def get_local_ip() -> str:
     """Try to determine the local IP address of the machine."""
@@ -9,8 +11,8 @@ def get_local_ip() -> str:
         local_ip_address = socket.gethostbyname(socket.gethostname())
         if not local_ip_address.startswith('127.'):
             return local_ip_address
-    except OSError:
-        pass
+    except socket.gaierror:
+        raise exception(-4013)  # DNS Error
 
     # Connecting to UDP address does not send packets.
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
