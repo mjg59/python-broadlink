e5fb09d97161a72f7e2c3ae9393924421d86a7c9
Felipe Martins Diel <felipe-diel@hotmail.com>
2020-09-23 21:29:42 -0300

    Clean up alarm kit

--- broadlink/alarm.py
+++ broadlink/alarm.py
@@ -1,16 +1,16 @@
+"""Support for alarm kits."""
 from .device import device
 from .exceptions import check_error
 
 
-S1C_SENSORS_TYPES = {
-    0x31: 'Door Sensor',  # 49 as hex
-    0x91: 'Key Fob',  # 145 as hex, as serial on fob corpse
-    0x21: 'Motion Sensor'  # 33 as hex
-}
-
-
 class S1C(device):
     """Controls a Broadlink S1C."""
+
+    _SENSORS_TYPES = {
+        0x31: 'Door Sensor',  # 49 as hex
+        0x91: 'Key Fob',  # 145 as hex, as serial on fob corpse
+        0x21: 'Motion Sensor'  # 33 as hex
+    }
 
     def __init__(self, *args, **kwargs) -> None:
         """Initialize the controller."""
@@ -27,30 +27,22 @@ class S1C(device):
         if not payload:
             return None
         count = payload[0x4]
-        sensors = payload[0x6:]
-        sensors_a = [bytearray(sensors[i * 83:(i + 1) * 83]) for i in range(len(sensors) // 83)]
-
-        sens_res = []
-        for sens in sensors_a:
-            status = sens[0]
-            _name = sens[4:26].decode()
-            _order = sens[1]
-            _type = sens[3]
-            _serial = sens[26:30].hex()
-
-            type_str = S1C_SENSORS_TYPES.get(_type, 'Unknown')
-
-            r = {
-                'status': status,
-                'name': _name.strip('\x00'),
-                'type': type_str,
-                'order': _order,
-                'serial': _serial,
-            }
-            if r['serial'] != '00000000':
-                sens_res.append(r)
-        result = {
+        sensor_data = payload[0x6:]
+        sensors = [
+            bytearray(sensor_data[i * 83:(i + 1) * 83])
+            for i in range(len(sensor_data) // 83)
+        ]
+        return {
             'count': count,
-            'sensors': sens_res
+            'sensors': [
+                {
+                    'status': sensor[0],
+                    'name': sensor[4:26].decode().strip('\x00'),
+                    'type': self._SENSORS_TYPES.get(sensor[3], 'Unknown'),
+                    'order': sensor[1],
+                    'serial': sensor[26:30].hex(),
+                }
+                for sensor in sensors
+                if any(sensor[26:30])
+            ]
         }
-        return result
