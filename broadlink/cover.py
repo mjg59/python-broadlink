0de42b1e0da07bdd2a639077aae3d8cc8849ac81
burjakremen <55150081+burjakremen@users.noreply.github.com>
2020-09-25 15:20:56 +0300

    Update cover.py
    Adding support for a Dooya DT360E-45/20 (Device ID: 4F6E)

--- broadlink/cover.py
+++ broadlink/cover.py
@@ -58,3 +58,55 @@ class dooya(device):
                 time.sleep(0.2)
                 current = self.get_percentage()
         self.stop()
+
+ class dooya_new(device):
+    """Controls a Dooya curtain motor."""
+
+    def __init__(self, *args, **kwargs) -> None:
+        """Initialize the controller."""
+        device.__init__(self, *args, **kwargs)
+        self.type = "Dooya DT360E(4F6E)"
+
+    def _send(self, magic1: int, magic2: int, magic3:int, magic4:int) -> int:
+        """Send a packet to the device."""
+        packet = bytearray(32)
+        packet[0] = 0x16
+        packet[2] = 0xa5
+        packet[3] = 0xa5
+        packet[4] = 0x5a
+        packet[5] = 0x5a
+        packet[6] = magic1
+        packet[7] = magic2
+        packet[8] = 0x02
+        packet[9] = 0x0b
+        packet[10] = 0x0a
+        packet[15] = magic3
+        packet[16] = magic4
+        response = self.send_packet(0x6a, packet)
+        check_error(response[0x22:0x24])
+        payload = self.decrypt(response[0x38:])
+        return payload[17]
+
+    def open(self) -> int:
+        """Open the curtain."""
+        return self._send(0xc5, 0xc0, 0x01, 0x00)
+
+    def close(self) -> int:
+        """Close the curtain."""
+        return self._send(0xc6, 0xc0, 0x02, 0x00)
+
+    def stop(self) -> int:
+        """Stop the curtain."""
+        return self._send(0xc7, 0xc0, 0x03, 0x00)
+
+    def get_percentage(self) -> int:
+        """Return the position of the curtain."""
+        return self._send(0xca, 0xc0 ,0x06, 0x5d)
+
+    def set_percentage_and_wait(self, new_percentage) -> int:
+        new_percent_hex = struct.pack('<H', 49357+new_percentage)
+        magic1 = new_percent_hex[0]
+        magic2 = new_percent_hex[1]
+        magic3 = 0x09
+        magic4 = new_percentage
+        return self._send(magic1, magic2, magic3, magic4)
