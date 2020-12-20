6cb0ae244d2c466cd6cfd5d23f3dd1a34f29113a
Felipe Martins Diel <41558831+felipediel@users.noreply.github.com>
2020-12-20 18:36:22 -0300

    Add support for BG Electrical AHC/U-01 (0x51E2)

--- broadlink/__init__.py
+++ broadlink/__init__.py
@@ -48,6 +48,7 @@ SUPPORTED_TYPES = {
     0x7D11: (sp4, "SP mini 3", "Broadlink"),
     0xA56A: (sp4, "MCB1", "Broadlink"),
     0xA589: (sp4, "SP4L-UK", "Broadlink"),
+    0x51E2: (sp4b, "AHC/U-01", "BG Electrical"),
     0x6113: (sp4b, "SCB1E", "Broadlink"),
     0x618B: (sp4b, "SP4L-EU", "Broadlink"),
     0x6489: (sp4b, "SP4L-AU", "Broadlink"),
