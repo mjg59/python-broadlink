941cc4d15270440d9a499a8422d44ab5fc62faff
Felipe Martins Diel <felipe-diel@hotmail.com>
2021-04-01 14:59:11 -0300

    Encapsulate crc16() to avoid global

--- broadlink/helpers.py
+++ broadlink/helpers.py
@@ -1,32 +1,38 @@
-"""Helper functions."""
+"""Helper functions and classes."""
 import typing as t
 
-_crc16_cache = {}
 
+class CRC16:  # pylint: disable=R0903
+    """Helps with CRC-16 calculation.
 
-def crc16(
-    sequence: t.Sequence[int],
-    polynomial: int = 0xA001,  # Default: Modbus CRC-16.
-    init_value: int = 0xFFFF,
-) -> int:
-    """Calculate the CRC-16 of a sequence of integers."""
-    global _crc16_cache
+    CRC tables are cached for performance.
+    """
 
-    try:
-        crc_table = _crc16_cache[polynomial]
-    except KeyError:
-        crc_table = []
-        for dividend in range(0, 256):
-            remainder = dividend
-            for _ in range(0, 8):
-                if remainder & 1:
-                    remainder = remainder >> 1 ^ polynomial
-                else:
-                    remainder = remainder >> 1
-            crc_table.append(remainder)
-        _crc16_cache[polynomial] = crc_table
+    _cache = {}
 
-    crc = init_value
-    for item in sequence:
-        crc = crc >> 8 ^ crc_table[(crc ^ item) & 0xFF]
-    return crc
+    @classmethod
+    def calculate(
+        cls,
+        sequence: t.Sequence[int],
+        polynomial: int = 0xA001,  # Modbus CRC-16.
+        init_value: int = 0xFFFF,
+    ) -> int:
+        """Calculate the CRC-16 of a sequence of integers."""
+        try:
+            crc_table = cls._cache[polynomial]
+        except KeyError:
+            crc_table = []
+            for dividend in range(0, 256):
+                remainder = dividend
+                for _ in range(0, 8):
+                    if remainder & 1:
+                        remainder = remainder >> 1 ^ polynomial
+                    else:
+                        remainder = remainder >> 1
+                crc_table.append(remainder)
+            cls._cache[polynomial] = crc_table
+
+        crc = init_value
+        for item in sequence:
+            crc = crc >> 8 ^ crc_table[(crc ^ item) & 0xFF]
+        return crc
