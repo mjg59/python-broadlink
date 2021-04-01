ac1786ebb766189dcf48352c889ca3289dff2729
Felipe Martins Diel <felipe-diel@hotmail.com>
2021-04-01 00:04:29 -0300

    Remove unnecessary import

--- broadlink/helpers.py
+++ broadlink/helpers.py
@@ -1,6 +1,4 @@
 """Helper functions."""
-from ctypes import c_ushort
-
 
 def crc16(sequence: bytes) -> int:
     """Calculate the CRC-16 of a byte string."""
@@ -8,19 +6,19 @@ def crc16(sequence: bytes) -> int:
     polynomial = 0xA001
 
     for dividend in range(0, 256):
-        remainder = c_ushort(dividend).value
+        remainder = dividend
         for _ in range(0, 8):
             if remainder & 0x0001:
-                remainder = c_ushort(remainder >> 1).value ^ polynomial
+                remainder = remainder >> 1 & 0xFFFF ^ polynomial
             else:
-                remainder = c_ushort(remainder >> 1).value
+                remainder = remainder >> 1 & 0xFFFF
         crc_table.append(hex(remainder))
 
     crc = 0xFFFF
 
     for item in sequence:
         index = (crc ^ item) & 0x00FF
-        base = c_ushort(crc >> 8).value
+        base = crc >> 8 & 0xFFFF
         crc = base ^ int(crc_table[index], 0)
 
     return crc
