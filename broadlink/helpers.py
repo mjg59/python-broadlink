8362d67b953e383093ceb7232f5820cab6344891
Felipe Martins Diel <felipe-diel@hotmail.com>
2021-03-31 23:56:23 -0300

    Apply PEP-8 naming conventions

--- broadlink/helpers.py
+++ broadlink/helpers.py
@@ -2,25 +2,25 @@
 from ctypes import c_ushort
 
 
-def crc16(input_data: bytes) -> int:
+def crc16(sequence: bytes) -> int:
     """Calculate the CRC-16 of a byte string."""
-    crc16_tab = []
-    crc16_constant = 0xA001
+    crc_table = []
+    polynomial = 0xA001
 
-    for i in range(0, 256):
-        crc = c_ushort(i).value
-        for j in range(0, 8):
-            if crc & 0x0001:
-                crc = c_ushort(crc >> 1).value ^ crc16_constant
+    for dividend in range(0, 256):
+        remainder = c_ushort(dividend).value
+        for _ in range(0, 8):
+            if remainder & 0x0001:
+                remainder = c_ushort(remainder >> 1).value ^ polynomial
             else:
-                crc = c_ushort(crc >> 1).value
-        crc16_tab.append(hex(crc))
+                remainder = c_ushort(remainder >> 1).value
+        crc_table.append(hex(remainder))
 
-    crcValue = 0xFFFF
+    crc = 0xFFFF
 
-    for c in input_data:
-        tmp = crcValue ^ c
-        rotated = c_ushort(crcValue >> 8).value
-        crcValue = rotated ^ int(crc16_tab[(tmp & 0x00FF)], 0)
+    for item in sequence:
+        index = (crc ^ item) & 0x00FF
+        base = c_ushort(crc >> 8).value
+        crc = base ^ int(crc_table[index], 0)
 
-    return crcValue
+    return crc
