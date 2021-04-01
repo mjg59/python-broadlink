f1293076c6d1862f28346a80ea5af0f3b135f623
Felipe Martins Diel <felipe-diel@hotmail.com>
2021-04-01 01:00:11 -0300

    Store the CRC-16 table for performance

--- broadlink/helpers.py
+++ broadlink/helpers.py
@@ -1,6 +1,8 @@
 """Helper functions."""
 import typing as t
 
+_crc16_cache = {}
+
 
 def crc16(
     sequence: t.Sequence[int],
@@ -8,15 +10,21 @@ def crc16(
     init_value=0xFFFF,
 ) -> int:
     """Calculate the CRC-16 of a byte string."""
-    crc_table = []
-    for dividend in range(0, 256):
-        remainder = dividend
-        for _ in range(0, 8):
-            if remainder & 1:
-                remainder = remainder >> 1 ^ polynomial
-            else:
-                remainder = remainder >> 1
-        crc_table.append(remainder)
+    global _crc16_cache
+
+    try:
+        crc_table = _crc16_cache[polynomial]
+    except KeyError:
+        crc_table = []
+        for dividend in range(0, 256):
+            remainder = dividend
+            for _ in range(0, 8):
+                if remainder & 1:
+                    remainder = remainder >> 1 ^ polynomial
+                else:
+                    remainder = remainder >> 1
+            crc_table.append(remainder)
+        _crc16_cache[polynomial] = crc_table
 
     crc = init_value
     for item in sequence:
