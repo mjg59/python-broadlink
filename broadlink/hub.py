f794fb0d4b74aca2c00ba77b49a94bb838c5dc94
Felipe Martins Diel <41558831+felipediel@users.noreply.github.com>
2022-03-19 17:44:08 -0300

    Improve logic of get_subdevices()
    Prevents infinite loop.

--- broadlink/hub.py
+++ broadlink/hub.py
@@ -5,45 +5,42 @@ import json
 from . import exceptions as e
 from .device import Device
 
+
 class s3(Device):
     """Controls a Broadlink S3."""
 
     TYPE = "S3"
-            
-    def get_subdevices(self) -> dict:
+    MAX_SUBDEVICES = 8
+
+    def get_subdevices(self) -> list:
         """Return the lit of sub devices."""
         sub_devices = []
-        get_subdevices = True
-        total = 0
-        state = {"count":5,"index":0}
-        
-        while(get_subdevices):
+        step = 5
+
+        for index in range(0, self.MAX_SUBDEVICES, step):
+            state = {"count": step, "index": index}
             packet = self._encode(14, state)
             resp = self.send_packet(0x6a, packet)
             e.check_error(resp[0x22:0x24])
             resp = self._decode(resp)
-            
+
             sub_devices.extend(resp["list"])
-            total = resp["total"]
-            
-            if len(sub_devices) == total:
-                get_subdevices = False
-            else:
-                state["index"] += 5
-            
-        return(sub_devices)
+            if len(sub_devices) == resp["total"]:
+                break
+
+        return sub_devices
 
     def get_state(self,did: str = None) -> dict:
         """Return the power state of the device."""
         state = {}
         if did is not None:
             state["did"] = did
-        
+
         packet = self._encode(1, state)
         response = self.send_packet(0x6A, packet)
         e.check_error(response[0x22:0x24])
         return self._decode(response)
- 
+
     def set_state(
         self,
         did: str = None,
@@ -67,7 +64,7 @@ class s3(Device):
         e.check_error(response[0x22:0x24])
         return self._decode(response)
 
-    
+
     def _encode(self, flag: int, state: dict) -> bytes:
         """Encode a JSON packet."""
         # flag: 1 for reading, 2 for writing.
@@ -78,10 +75,10 @@ class s3(Device):
         checksum = sum(packet, 0xBEAF) & 0xFFFF
         packet[0x04:0x06] = checksum.to_bytes(2, "little")
         return packet
-    
+
     def _decode(self, response: bytes) -> dict:
         """Decode a JSON packet."""
         payload = self.decrypt(response[0x38:])
         js_len = struct.unpack_from("<I", payload, 0x08)[0]
         state = json.loads(payload[0x0C : 0x0C + js_len])
-        return state
+        return state 
