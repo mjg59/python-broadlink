472b304b17d75a06093aafb5657c2639c8c6bddf
dtwilliamson <dennis@netstrata.com>
2022-05-02 09:31:48 -0500

    add Bestcon RM4C 0x5218

--- broadlink/__init__.py
+++ broadlink/__init__.py
@@ -125,6 +125,7 @@ SUPPORTED_TYPES = {
     },
     rm4pro: {
         0x5213: ("RM4 pro", "Broadlink"),
+        0x5218: ("RM4C pro", "Bestcon"),
         0x6026: ("RM4 pro", "Broadlink"),
         0x6184: ("RM4C pro", "Broadlink"),
         0x61A2: ("RM4 pro", "Broadlink"),
